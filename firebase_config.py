7e17dd0c1390db0cffc55f6d6de774f7a5d6ead6
Leah Korol <leah03120@gmail.com>
2024-12-30 12:32:41 +0200

    Added Flask Firebase template from Flask-Firebase-Template
    Copied the code from the Flask-Firebase-Template repository by adjjunn.
    Adjusted the code to fit our Firebase configuration.
    Original repository URL: https://github.com/adjdunn/Flask-Firebase-Template.
    Licensed under the MIT License.

---
+++ firebase_config.py
@@ -0,0 +1,10 @@
+# This file contains the firebase configuration details.
+firebaseConfig = {
+  "apiKey": "AIzaSyAkV5g2KHDs2A8Lc8FzXDSnyvUSqrO3rpU",
+  "authDomain": "remez-ccf2d.firebaseapp.com",
+  "projectId": "remez-ccf2d",
+  "storageBucket": "remez-ccf2d.firebasestorage.app",
+  "messagingSenderId": "196822706306",
+  "appId": "1:196822706306:web:585d5833b7713d5b106c9b",
+  "measurementId": "G-RMKPP8SHD6"
+}
