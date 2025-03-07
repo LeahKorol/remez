b1dd7ed5b3a9226553f3e1ab20763168dcfc79f4
Talya Kazayof <talya8399@gmail.com>
2025-03-07 15:51:02 +0200

    Add a footer component for use it every time we will need prevent a duplicate code

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+      <footer className="bg-white bg-opacity-90 backdrop-blur-sm border-t border-gray-200 py-4 text-sm text-gray-500 text-center">
+        <div className="container mx-auto px-4">
+          <p className="mb-1">Created by Eng. Leah Korol and Eng. Talya Kazayof</p>
+          <p>In collaboration with Dr. Boris Gorelik</p>
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
