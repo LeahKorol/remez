78cd6af941b08fbdd11d2e522970bf800a4a3704
Leah Korol <leah03120@gmail.com>
2025-03-26 00:58:40 +0200

    Add a command to download FAERS data files. Closes #26

---
+++ backend/analysis/management/commands/download_faers_data.py
@@ -0,0 +1,104 @@
+"""
+This file is adapted from:
+https://github.com/bgbg/faers_analysis/blob/main/src/download_faers_data.py
+It has been modified to work with Django management commands instead of defopt.
+"""
+
+import logging
+import os
+import shutil
+import urllib.request
+from multiprocessing.dummy import Pool as ThreadPool
+import tqdm
+
+from django.core.management.base import BaseCommand, CommandError
+from analysis.faers_analysis.src.utils import Quarter, generate_quarters
+
+
+class Command(BaseCommand):
+    help = "Download FAERS quarterly CSV files"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "year_q_from",
+            type=str,
+            help='XXXXqQ, where XXXX is the year, q is the literal "q" and Q is 1, 2, 3 or 4',
+        )
+        parser.add_argument(
+            "year_q_to",
+            type=str,
+            help='XXXXqQ, where XXXX is the year, q is the literal "q" and Q is 1, 2, 3 or 4',
+        )
+        parser.add_argument(
+            "--dir_out",
+            type=str,
+            help="Output directory",
+        )
+        parser.add_argument("--threads", type=int, help="N of parallel threads")
+
+        parser.add_argument(
+            "--clean_on_failure",
+            type=bool,
+            help="Delete the output directory if the command fails",
+        )
+
+    def handle(self, *args, **options):
+        year_q_from = options["year_q_from"]
+        year_q_to = options["year_q_to"]
+        dir_out = os.path.abspath(
+            options.get("dir_out", "analysis/management/commands/output")
+        )
+        threads = options.get("threads", 4)
+        clean_on_failure = options.get("clean_on_failure", True)
+
+        dir_out = os.path.abspath(dir_out)
+        os.makedirs(dir_out, exist_ok=True)
+        try:
+            q_first = Quarter(year_q_from)
+            q_last = Quarter(year_q_to)
+            urls = []
+            for q in generate_quarters(q_first, q_last):
+                urls.extend(Command.quarter_urls(q))
+
+            print(f"will download {len(urls)} urls")
+            with ThreadPool(threads) as pool:
+                _ = list(
+                    tqdm.tqdm(
+                        pool.imap(lambda url: Command.download_url(url, dir_out), urls),
+                        total=len(urls),
+                    )
+                )
+        except Exception as err:
+            if clean_on_failure:
+                shutil.rmtree(dir_out)
+            raise CommandError(str(err))
+
+    @staticmethod
+    def quarter_urls(quarter):
+        ret = []
+        year = quarter.year
+        yearquarter = str(quarter)
+        what = [
+            "demo",
+            "drug",
+            "reac",
+            "outc",
+            # "indi",
+            # "ther",
+        ]
+        for w in what:
+            tmplt = f"https://data.nber.org/fda/faers/{year}/{w}{yearquarter}.csv.zip"
+            ret.append(tmplt)
+        return ret
+
+    @staticmethod
+    def download_url(url, dir_out):
+        fn_out = os.path.split(url)[-1]
+        fn_out = os.path.join(dir_out, fn_out)
+        try:
+            urllib.request.urlretrieve(url, fn_out)
+        except Exception as err:
+            logging.error(f"Failed to download {url} to {fn_out} {err}")
+        else:
+            logging.info(f"Saved {fn_out}")
+            assert os.path.exists(fn_out)
