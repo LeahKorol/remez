bf9d233884da6bcd35ee6a9c921c85a85da1ec8f
Talya Kazayof <106309652+Talya2003@users.noreply.github.com>
2025-03-29 22:58:03 +0300

    Delete backend directory

--- backend/analysis/urls.py
+++
@@ -1,6 +0,0 @@
-from analysis.views import QueryViewSet
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register(r"queries", QueryViewSet, basename="query")
-urlpatterns = router.urls
