8c11b20deb685f55f09d22c54c82aa5acc8ec555
Talya Kazayof <talya8399@gmail.com>
2025-03-29 23:49:25 +0300

    Basic initialization of the main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+// import logo from '../public/r-icon.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1 className="welcome-text">Welcome to REMEZ</h1>
+        <p className="App-description">
+          Advanced Medication Interaction Analysis
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
