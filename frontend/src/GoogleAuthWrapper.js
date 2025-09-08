01aee2f8038b790e59e8a01884b5b11a7a755635
Talya <talya8399@gmail.com>
2025-09-08 15:21:13 +0300

    Add Google authentication wrapper component with One Tap initialization

---
+++ frontend/src/GoogleAuthWrapper.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { toast } from 'react-toastify';
+import { useGoogleAuth } from './GoogleAuth';
+
+const GoogleAuthWrapper = () => {
+  const { initializeOneTap, signInWithGoogle, isLoading } = useGoogleAuth();
+
+  useEffect(() => {
+    // try to initialize One Tap on component mount
+    initializeOneTap();
+  }, []);
+
+  const handleGoogleClick = () => {
+    // allways enable to try to sign in with Google on button click
+    signInWithGoogle();
+  };
+
+  return (
+    <div>
+      <button
+        onClick={handleGoogleClick}
+        disabled={isLoading}
+        className="google-auth-button"
+      >
+        {isLoading ? 'Connecting...' : 'Login with Google'}
+      </button>
+    </div>
+  );
+};
+
+export default GoogleAuthWrapper;
