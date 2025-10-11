b89453b04bcf482944f9961814150d232b0caeaf
Talya Kazayof <106309652+Talya2003@users.noreply.github.com>
2025-10-11 23:36:49 +0300

    Add links to privacy-policy and about pages from HomePage.js

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -123,8 +123,13 @@ function Home() {
                 <button className="begin-btn" onClick={handleLoginClick}>Login to Begin</button>
 
                 <div className="footer">
-                    <p>Created by Eng. Leah Korol and Eng. Talya Kazayof</p>
-                    <p>In collaboration with Dr. Boris Gorelik</p>
+                  <p>Created by Eng. Leah Korol and Eng. Talya Kazayof</p>
+                  <p>In collaboration with Dr. Boris Gorelik</p>
+                  <div className="footer-links">
+                    <a href="/about-research" className="footer-link">About the Research</a>
+                    <span className="separator">|</span>
+                    <a href="/privacy-policy" className="footer-link">Privacy Policy</a>
+                  </div>
                 </div>
             </div>
         </div>
